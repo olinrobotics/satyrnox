346e927987f0e00a08a49bc500b195e92c819659
Sabrina Pereira <sabrina.pereira@students.olin.edu>
2017-12-17 12:13:52 -0500

    Changed stuff

--- EmotionalReactions/EmotionalReactions.ino
+++ EmotionalReactions/EmotionalReactions.ino
@@ -13,6 +13,8 @@ Servo HeadLift;
 
 //Pin button is connected to
 const int HandButton = 10;
+const int RArmLiftPin = 5;
+const int LArmLiftPin = 6;
 
 //Preset Servo angle settings
 //const int initialPos;
@@ -28,13 +30,13 @@ int pos;
 
 
 void setup() {
-  servo1.attach(servo1Pin);
-  servo2.attach(servo2Pin); // attaches the servo on pin 3 and 4 to servo object
+  RArmLift.attach(RArmLiftPin);
+  LArmLift.attach(LArmLiftPin); // attaches the servo on pin 3 and 4 to servo object
 
-  //servo1.write(pos) tells servo to go to position 'pos' (degrees)
+  //RArmLift.write(pos) tells servo to go to position 'pos' (degrees)
 
-  servo1.write(readyPos);
-  servo2.write(readyPos);
+  RArmLift.write(readyPos);
+  LArmLift.write(readyPos);
   
   //delay(15); // waits x time for the servo to reach the position
 
@@ -54,14 +56,14 @@ void wiggleArms()
 {
 
   for (pos = readyPos; pos <= finalPos; pos -= 5) { // goes from readyPos to finalPos
-    servo1.write(pos);              
-    servo2.write(pos);                // tell servo to go to position in variable 'pos'
+    RArmLift.write(pos);              
+    LArmLift.write(pos);                // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
   for (pos = finalPos; pos >= readyPos; pos += 5) { // goes from finalPos to readyPos
     // in steps of 1 degree
-    servo1.write(pos);              
-    servo2.write(pos);                // tell servo to go to position in variable 'pos'
+    RArmLift.write(pos);              
+    LArmLift.write(pos);                // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
   
