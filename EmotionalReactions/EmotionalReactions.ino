9f5f8185469c795b1a30c55ebd2407b645057ce6
Sabrina Pereira <sabrina.pereira@students.olin.edu>
2017-12-17 12:10:25 -0500

    Addind new program to be written

---
+++ EmotionalReactions/EmotionalReactions.ino
@@ -0,0 +1,73 @@
+
+#include <Servo.h> //Servo library
+
+//Creating servo objects
+
+Servo BaseRot;
+Servo RArmRot;
+Servo LArmRot;
+Servo RArmLift;
+Servo LArmLift;
+Servo HeadRot;
+Servo HeadLift;
+
+//Pin button is connected to
+const int HandButton = 10;
+
+//Preset Servo angle settings
+//const int initialPos;
+
+const int readyPos = 170;
+const int finalPos = 110;
+
+
+//Variables relating to state of button/servo
+
+int pos;
+
+
+
+void setup() {
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin); // attaches the servo on pin 3 and 4 to servo object
+
+  //servo1.write(pos) tells servo to go to position 'pos' (degrees)
+
+  servo1.write(readyPos);
+  servo2.write(readyPos);
+  
+  //delay(15); // waits x time for the servo to reach the position
+
+}
+
+void loop() {
+
+  for (int i = 1; i <= 3; i++)
+  {
+    wiggleArms();
+  }
+}
+
+
+
+void wiggleArms()
+{
+
+  for (pos = readyPos; pos <= finalPos; pos -= 5) { // goes from readyPos to finalPos
+    servo1.write(pos);              
+    servo2.write(pos);                // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = finalPos; pos >= readyPos; pos += 5) { // goes from finalPos to readyPos
+    // in steps of 1 degree
+    servo1.write(pos);              
+    servo2.write(pos);                // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  
+
+
+}
+
+
+
