ce33588059732a1822e6b21fcb4864e409f23c75
Sabrina Pereira <sabrina.pereira@students.olin.edu>
2017-12-17 12:01:25 -0500

    Updated

--- HighFive/HighFive.ino
+++ HighFive/HighFive.ino
@@ -1,21 +1,28 @@
 
 #include <Servo.h> //Servo library
 
-Servo servo1; //Creating servo object
+//Creating servo objects
 
-//Pins button and servo are connected to
-const int buttonPin = 2;
-const int servo1Pin = 9;
+Servo BaseRot;
+Servo RArmRot;
+Servo LArmRot;
+Servo RArmLift;
+Servo LArmLift;
+Servo HeadRot;
+Servo HeadLift;
+
+//Pin button is connected to
+const int HandButton = 10;
 
 //Preset Servo angle settings
 //const int initialPos;
-const int readyPos;
-const int highFivePos;
-const int finalPos;
+const int readyPos = 20;
+const int highFivePos = 60;
+const int finalPos = 30;
 
 
 //Variables relating to state of button/servo
-bool highFiveOccuring = false;
+bool highFiveOccuring = true;
 bool highFiveComplete = false;
 int buttonState = 0;
 int pos = readyPos;
@@ -23,19 +30,20 @@ int pos = readyPos;
 
 
 void setup() {
-  servo1.attach(servo1Pin); // attaches the servo on pin 9 to servo object
-  pinMode(buttonPin, INPUT); // initialize the pin for button as input
+  LArmLift.attach(LArmLift); // attaches the servo on pin 9 to servo object
+  pinMode(HandButton, INPUT); // initialize the pin for button as input
 
-  //servo1.write(pos) tells servo to go to position 'pos' (degrees)
+  //LArmLift.write(pos) tells servo to go to position 'pos' (degrees)
 
-  servo1.write(readyPos); //servo1.write(intitialPos);
+  LArmLift.write(readyPos); //LArmLift.write(intitialPos);
   delay(500); // waits x time for the servo to reach the position
 
 }
 
-void loop() {
-highFive(170, 110, 170); //random arbritary values put in
-delay(1000);
+void loop() { //loop for testing
+highFive(readyPos, highFivePos, finalPos);
+delay(5000); 
+highFiveOccuring = true;
 }
 
 
@@ -46,17 +54,17 @@ void highFive(const int readyPos, const int highFivePos, const int finalPos)
   highFiveOccuring == true; //to remove at some point
 
 
-  while ( servo1.read() > highFivePos && highFiveComplete == false && highFiveOccuring == true)
+  while ( LArmLift.read() > highFivePos && highFiveComplete == false && highFiveOccuring == true)
   {
     // goes from readyPos to highFivePos in steps of 1 degree
 
-    buttonState = digitalRead(buttonPin);
-    servo1.write(pos);              // tell servo to go to position in variable 'pos'
+    buttonState = digitalRead(HandButton);
+    LArmLift.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
     if (buttonState == HIGH)
     {
       highFiveComplete = true;
-      pos = servo1.read();
+      pos = LArmLift.read();
       break;
     }
 
@@ -65,9 +73,9 @@ void highFive(const int readyPos, const int highFivePos, const int finalPos)
   }
 
 
-  while (servo1.read() < finalPos && highFiveOccuring == true)
+  while (LArmLift.read() < finalPos && highFiveOccuring == true)
   {
-    servo1.write(pos);
+    LArmLift.write(pos);
     delay(15);
     pos += 1;
 
